0b9d715d1a637f04d8048e240d26e699d7b38d78
lelkok228 <abalymov.iv@phystech.edu>
2020-05-17 00:20:41 +0300

    main.py commit

--- main.py
+++ main.py
@@ -1,177 +1,176 @@
 import json
 import sys
+import argparse
 
 
-def read_s(in_file):
-    res = ''
-    if in_file == 'console':
-        for line in sys.stdin:
-            res += line
+def read_string(input_file):
+    input_string = ''
+    if input_file:
+        with open(input_file, 'r') as in_:
+            input_string = in_.read()
     else:
-        with open(in_file, 'r') as in_:
-            res = in_.read()
+        for line in sys.stdin:
+            input_string += line
+    return input_string
+
+
+def write_string(output_string, output_file):
+    if output_file:
+        with open(output_file, 'w') as out_:
+            out_.write(output_string)
+    else:
+        print(output_string)
+
+
+def read_frequency(frequency_file):
+    with open(frequency_file, 'r') as freq_file:
+        frequency = json.load(freq_file)
+    return frequency
+
+
+def get_alphabet():
+    alphabet = [chr(i) for i in range(ord('a'), ord('z') + 1)]
+    return alphabet
+
+
+def encode_caesar(line, key):
+    alphabet = get_alphabet()
+    res = ''
+
+    for letter in line:
+        if letter in alphabet:
+            res += alphabet[(alphabet.index(letter) + key) % len(alphabet)]
+        elif letter.lower() in alphabet:
+            res += alphabet[(alphabet.index(letter.lower()) + key) % len(alphabet)].upper()
+        else:
+            res += letter
+
     return res
 
 
-def write_s(s, out_file):
-    if out_file == 'console':
-        print(s)
-    else:
-        with open(out_file, 'w') as out_:
-            out_.write(s)
-
-
-def encode_caesar(key, in_file, out_file):
-    global alphabet
-
-    s = read_s(in_file)
+def decode_caesar(line, key):
+    alphabet = get_alphabet()
 
     res = ''
-    for i in s:
-        if i in alphabet:
-            res += alphabet[(alphabet.index(i) + key) % len(alphabet)]
-        elif i.lower() in alphabet:
-            res += alphabet[(alphabet.index(i.lower()) + key) % len(alphabet)].upper()
+    for letter in line:
+        if letter in alphabet:
+            res += alphabet[(alphabet.index(letter) - key) % len(alphabet)]
+        elif letter.lower() in alphabet:
+            res += alphabet[(alphabet.index(letter.lower()) - key) % len(alphabet)].upper()
         else:
-            res += i
+            res += letter
 
-    write_s(res, out_file)
-
-
-def decode_caesar(key, in_file, out_file):
-    global alphabet
-
-    s = read_s(in_file)
-
-    res = ''
-    for i in s:
-        if i in alphabet:
-            res += alphabet[(alphabet.index(i) - key) % len(alphabet)]
-        elif i.lower() in alphabet:
-            res += alphabet[(alphabet.index(i.lower()) - key) % len(alphabet)].upper()
-        else:
-            res += i
-
-    write_s(res, out_file)
+    return res
 
 
-def encode_vigenere(key, in_file, out_file):
-    global alphabet
-
+def encode_vigenere(line, key):
+    alphabet = get_alphabet()
     key = key.lower()
-    s = read_s(in_file)
 
     res = ''
-    for i in range(len(s)):
-        if s[i] in alphabet:
-            res += alphabet[(alphabet.index(s[i]) + alphabet.index(key[i % len(key)])) % len(alphabet)]
-        elif s[i].lower() in alphabet:
-            res += alphabet[(alphabet.index(s[i].lower()) + alphabet.index(key[i % len(key)])) % len(alphabet)].upper()
+    for i in range(len(line)):
+        if line[i] in alphabet:
+            res += alphabet[(alphabet.index(line[i]) + alphabet.index(key[i % len(key)])) % len(alphabet)]
+        elif line[i].lower() in alphabet:
+            index_line_i_lower = alphabet.index(line[i].lower())
+            res += alphabet[(index_line_i_lower + alphabet.index(key[i % len(key)])) % len(alphabet)].upper()
         else:
-            res += s[i]
+            res += line[i]
 
-    write_s(res, out_file)
+    return res
 
 
-def decode_vigenere(key, in_file, out_file):
-    global alphabet
-
+def decode_vigenere(line, key):
+    alphabet = get_alphabet()
     key = key.lower()
-    s = read_s(in_file)
 
     res = ''
-    for i in range(len(s)):
-        if s[i] in alphabet:
-            res += alphabet[(alphabet.index(s[i]) - alphabet.index(key[i % len(key)])) % len(alphabet)]
-        elif s[i].lower() in alphabet:
-            res += alphabet[(alphabet.index(s[i].lower()) - alphabet.index(key[i % len(key)])) % len(alphabet)].upper()
+    for i in range(len(line)):
+        if line[i] in alphabet:
+            res += alphabet[(alphabet.index(line[i])) - alphabet.index(key[i % len(key)]) % len(alphabet)]
+        elif line[i].lower() in alphabet:
+            index_line_i_lower = alphabet.index(line[i].lower())
+            res += alphabet[(index_line_i_lower - alphabet.index(key[i % len(key)])) % len(alphabet)].upper()
         else:
-            res += s[i]
+            res += line[i]
 
-    write_s(res, out_file)
+    return res
 
 
-def count_frequency(in_file, out_file):
-    global alphabet
+def get_frequency(line):
+    alphabet = get_alphabet()
+    line = line.lower()
 
-    s = read_s(in_file)
-    s = s.lower()
-
-    number_of_letters = sum(s.count(i) for i in alphabet)
+    number_of_letters = sum(line.count(letter) for letter in alphabet)
     frequency = {}
 
-    for i in alphabet:
+    for letter in alphabet:
         if number_of_letters != 0:
-            frequency[i] = s.count(i) / number_of_letters
+            frequency[letter] = line.count(letter) / number_of_letters
         else:
-            frequency[i] = 0
+            frequency[letter] = 0
 
-    write_s(json.dumps(frequency), out_file)
+    return frequency
 
 
-def hack(in_file, out_file, frequency_file):
-    global alphabet
+def get_index_of_upper(line):
+    index_of_upper = []
+    for i in range(len(line)):
+    	if line[i].isupper():
+    		index_of_upper.append(i)
+    return index_of_upper
 
-    s = read_s(in_file)
 
-    number_of_letters = 0
-    ind_of_upper = []
-    for i in range(len(s)):
-        if s[i].lower() in alphabet:
-            number_of_letters += 1
-        if s[i].isupper():
-            ind_of_upper.append(i)
-    s = s.lower()
-
-    with open(frequency_file, 'r') as freq_file:
-        freq = json.load(freq_file)
+def hack(line, frequency):
+    alphabet = get_alphabet()
+    index_of_upper = get_index_of_upper(line)
+    line = line.lower()
+    number_of_letters = sum(line.count(letter) for letter in alphabet)
 
     dist = [0] * len(alphabet)
     for shift in range(len(alphabet)):
-        new_s = ''
-        for i in s:
-            if i in alphabet:
-                new_s += alphabet[(alphabet.index(i) + shift) % len(alphabet)]
-            else:
-                new_s += i
+        new_line = encode_caesar(line, shift)
 
-        for i in alphabet:
+        for letter in alphabet:
             if number_of_letters != 0:
-                dist[shift] += (freq[i] - new_s.count(i) / number_of_letters) ** 2
+                dist[shift] += (frequency[letter] - new_line.count(letter) / number_of_letters) ** 2
 
     res_shift = dist.index(min(dist))
-    res = ''
-    for i in range(len(s)):
-        if s[i] in alphabet:
-            if i in ind_of_upper:
-                res += alphabet[(alphabet.index(s[i]) + res_shift) % len(alphabet)].upper()
-            else:
-                res += alphabet[(alphabet.index(s[i]) + res_shift) % len(alphabet)]
-        else:
-            res += s[i]
+    res_line = encode_caesar(line, res_shift)
+    for i in index_of_upper:
+    	res_line = res_line[:i] + res_line[i].upper() + res_line[i + 1:]
 
-    write_s(res, out_file)
+    return res_line
 
 
 if __name__ == '__main__':
-    alphabet = []
-    for i in range(ord('a'), ord('z') + 1):
-        alphabet.append(chr(i))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('mode')
+    parser.add_argument('--input_file')
+    parser.add_argument('--output_file')
+    parser.add_argument('--cipher')
+    parser.add_argument('--key')
+    parser.add_argument('--frequency_file')
+    args = parser.parse_args()  
 
-    if sys.argv[1] == 'encode':
-        if sys.argv[2] == 'caesar':
-            encode_caesar(int(sys.argv[3]), sys.argv[4], sys.argv[5])
-        elif sys.argv[2] == 'vigenere':
-            encode_vigenere(sys.argv[3], sys.argv[4], sys.argv[5])
+    input_string = read_string(args.input_file)
+    if args.frequency_file:
+        frequency = read_frequency(args.frequency_file)
 
-    if sys.argv[1] == 'decode':
-        if sys.argv[2] == 'caesar':
-            decode_caesar(int(sys.argv[3]), sys.argv[4], sys.argv[5])
-        elif sys.argv[2] == 'vigenere':
-            decode_vigenere(sys.argv[3], sys.argv[4], sys.argv[5])
+    if args.mode == 'encode':
+        if args.cipher == 'caesar':
+            write_string(encode_caesar(input_string, int(args.key)), args.output_file)
+        elif args.cipher == 'vigenere':
+            write_string(encode_vigenere(input_string, args.key), args.output_file)
 
-    if sys.argv[1] == 'frequency':
-        count_frequency(sys.argv[2], sys.argv[3])
+    if args.mode == 'decode':
+        if args.cipher == 'caesar':
+            write_string(decode_caesar(input_string, int(args.key)), args.output_file)
+        elif args.cipher == 'vigenere':
+            write_string(decode_vigenere(input_string, args.key), args.output_file)
 
-    if sys.argv[1] == 'hack':
-        hack(sys.argv[2], sys.argv[3], sys.argv[4])
+    if args.mode == 'frequency':
+    	write_string(json.dumps(get_frequency(input_string)), args.output_file)
+
+    if args.mode == 'hack':
+    	write_string(hack(input_string, frequency), args.output_file)
